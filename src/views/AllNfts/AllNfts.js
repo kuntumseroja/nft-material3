af23849b12ca7a036cc897d09ebb76ad619a68e0
kuntum <>
2024-01-17 19:03:10 +0700

    pagination fix material

--- src/views/AllNfts/AllNfts.js
+++ src/views/AllNfts/AllNfts.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {
   Box,
   TextField,
+  Pagination,
 } from '@mui/material';
 //import Box from '@mui/material/Box';
 //import TextField from '@mui/material/TextField';
