ba32b739ce5ceee3c8709a66fff038e49aa554f0
kuntum <>
2024-01-29 18:31:16 +0700

    3 items each page

--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -254,7 +254,7 @@ const PortfolioGrid = ({ data = [], dataCsv = [], buttonShow, buttonAsset }) => 
       <Grid container spacing={4}>
         {data.map((item, i) => (
           // setNftName(`${item.name}`);
-          <Grid item xs={12} sm={6} md={6} key={i}>
+          <Grid item xs={12} sm={6} md={4} key={i}>
             <Box display={'block'} width={1} height={1}>
               <Box
                 key={i}
